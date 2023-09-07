83bbd746a4381822d4fcc514cf272104fe8a7cc2
Muhsin <muhzin029@gmail.com>
2023-09-07 18:46:38 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,60 @@
+data = {
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "!": "-.-.--",
+    "-": "-....-",
+    "/": "-..-.",
+    "@": ".--.-.",
+    "(": "-.--.",
+    ")": "-.--.-"
+}
+
+m_code = ""
+text = input("Enter the Text to enter and hit enter to get the result: ")
+# TODO: for loop the 'text' variable to the corresponding match case
+# TODO: if match found append it to the new string
+# TODO: print the new string
+
+for char in text:
+    if char not in data:
+        print(" Its NOT in!")
+        break
+    if char in data:
+        print(" Its in!")
