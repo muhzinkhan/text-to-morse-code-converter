12834da457818826c285cc97100f7f792758cd52
Muhsin <muhzin029@gmail.com>
2023-09-07 20:29:04 +0530

    Final Working Project

--- main.py
+++ main.py
@@ -43,24 +43,28 @@ data = {
     "/": "-..-.",
     "@": ".--.-.",
     "(": "-.--.",
-    ")": "-.--.-"
+    ")": "-.--.-",
+    " ": "/"
 }
 
-m_code = ""
-text = input("Enter the Text to enter and hit enter to get the result: ").lower()
-
-# TODO: for loop the 'text' variable to the corresponding match case
-# TODO: if match found append it to the new string
-# TODO: print the new string
-
-for char in text:
-    for m_char_key in data:
-        if char == m_char_key:
-            m_code += data.get(m_char_key)
-            print(m_char_key)
-            print(data.get(m_char_key))
-    print(char)
+morse_code = None
 
 
+def morse_converter(text):
+    m_code = ""
+    for char in text:
+        for m_char_key in data:
+            if char == m_char_key:
+                m_code += " " + data.get(m_char_key)
+            elif char not in data:
+                print("\nEntered text has unsupported characters.\n"
+                      "Please remove it or find another converter that does it.")
+                return None
+    return m_code[1:]
 
-print(m_code)
+
+while not morse_code:
+    input_text = input("\nEnter the Text and hit enter to get the result: ").lower()
+    morse_code = morse_converter(input_text)
+
+print(f"The morse code is :\n{morse_code}\n")
