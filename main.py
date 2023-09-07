fd81c4d664d73247fcd703df1cc0781ee01a55a3
Muhsin <muhzin029@gmail.com>
2023-09-07 19:23:41 +0530

    First working Prototype

--- main.py
+++ main.py
@@ -47,14 +47,20 @@ data = {
 }
 
 m_code = ""
-text = input("Enter the Text to enter and hit enter to get the result: ")
+text = input("Enter the Text to enter and hit enter to get the result: ").lower()
+
 # TODO: for loop the 'text' variable to the corresponding match case
 # TODO: if match found append it to the new string
 # TODO: print the new string
 
 for char in text:
-    if char not in data:
-        print(" Its NOT in!")
-        break
-    if char in data:
-        print(" Its in!")
+    for m_char_key in data:
+        if char == m_char_key:
+            m_code += data.get(m_char_key)
+            print(m_char_key)
+            print(data.get(m_char_key))
+    print(char)
+
+
+
+print(m_code)
